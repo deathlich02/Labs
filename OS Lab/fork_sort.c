8f782ac238ef4a239e0bea89542826897ae677b9
deathlich02 <165387253+deathlich02@users.noreply.github.com>
2024-10-22 19:44:36 +0530

    Update fork_sort.c

--- OS Lab/fork_sort.c
+++ OS Lab/fork_sort.c
@@ -1,83 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
-#include <errno.h>
-#define MAX_STRINGS 100
-#define MAX_LENGTH 100
-void bubbleSort(char arr[][MAX_LENGTH], int n) {
-int i, j;
-char temp[MAX_LENGTH];
-for (i = 0; i < n-1; i++) {
-for (j = 0; j < n-i-1; j++) {
-if (strcmp(arr[j], arr[j+1]) > 0) {
-strcpy(temp, arr[j]);
-strcpy(arr[j], arr[j+1]);
-strcpy(arr[j+1], temp);
+#include <unistd.h>
+#include <string.h>
+
+#define MAX 100
+
+void bubble(char strings[MAX][MAX], int n) {
+    char temp[MAX];
+    for(int i = 0; i < n-1; i++) {
+        for(int j = 0; j < n-i-1; j++) {
+            if(strcmp(strings[j], strings[j+1]) > 0) {
+                strcpy(temp, strings[j]);
+                strcpy(strings[j], strings[j+1]);
+                strcpy(strings[j+1], temp);
+            }
+        }
+    }
 }
+
+void selection(char strings[MAX][MAX], int n) {
+    int min;
+    char temp[MAX];
+    for(int i = 0; i < n-1; i++) {
+        min = i;
+        for(int j = i+1; j < n; j++) {
+            if(strcmp(strings[j], strings[min]) < 0) {
+                min = j;
+            }
+        }
+        strcpy(temp, strings[min]);
+        strcpy(strings[min], strings[i]);
+        strcpy(strings[i], temp);
+    }
 }
-}
-}
-void selectionSort(char arr[][MAX_LENGTH], int n) {
-int i, j, min_idx;
-char temp[MAX_LENGTH];
-for (i = 0; i < n-1; i++) {
-min_idx = i;
-for (j = i+1; j < n; j++) {
-if (strcmp(arr[j], arr[min_idx]) < 0) {
-min_idx = j;
-}
-}
-strcpy(temp, arr[min_idx]);
-strcpy(arr[min_idx], arr[i]);
-strcpy(arr[i], temp);
-}
-}
-int main(void) {
-int n;
-pid_t pid1, pid2;
-// Read number of strings
-printf("Enter the number of strings: ");
-scanf("%d", &n);
-// Read the strings
-char strings[MAX_STRINGS][MAX_LENGTH];
-printf("Enter the strings:\n");
-for (int i = 0; i < n; i++) {
-scanf("%s", strings[i]);
-}
-// Fork the first child process
-pid1 = fork();
-if (pid1 == 0) {
-// Child 1 process - perform bubble sort
-bubbleSort(strings, n);
-printf("Bubble sorted strings:\n");
-for (int i = 0; i < n; i++) {
-printf("%s\n", strings[i]);
-}
-exit(0);
-} else if (pid1 < 0) {
-// Fork failed
-perror("Fork failed for child 1");
-return 1;
-}
-// Fork the second child process
-pid2 = fork();
-if (pid2 == 0) {
-// Child 2 process - perform selection sort
-selectionSort(strings, n);
-printf("Selection sorted strings:\n");
-for (int i = 0; i < n; i++) {
-printf("%s\n", strings[i]);
-}
-exit(0);
-} else if (pid2 < 0) {
-// Fork failed
-perror("Fork failed for child 2");
-return 1;
-}
-// Parent process waits for one child to terminate
-int status;
-wait(&status); // Wait for one child to finish
-return 0;
+
+int main() {
+    char strings[MAX][MAX], strings_copy[MAX][MAX];
+    int n;
+    pid_t pid1, pid2;
+
+    printf("Enter the number of words: \n");
+    scanf("%d", &n);
+    printf("Enter the words: \n");
+    for(int i = 0; i < n; i++) {
+       scanf("%s", strings[i]);
+       strcpy(strings_copy[i], strings[i]);  // Make a copy of the original strings
+    }
+
+    // First child process for bubble sort
+    pid1 = fork();
+    if(pid1 == 0) {
+        bubble(strings, n);
+        printf("Bubble Sorted words are:\n");
+        for(int i = 0; i < n; i++)
+            printf("%s\n", strings[i]);  // Print each word with a newline
+        exit(0);
+    }
+    else if(pid1 < 0) {
+        perror("Fork failed\n");
+        return 0;
+    }
+
+    // Second child process for selection sort
+    pid2 = fork();
+    if(pid2 == 0) {
+        selection(strings_copy, n);  // Use the copied array for selection sort
+        printf("Selection Sorted words are:\n");
+        for(int i = 0; i < n; i++)
+            printf("%s\n", strings_copy[i]);  // Print each word with a newline
+        exit(0);
+    }
+    else if(pid2 < 0) {
+        perror("Fork failed\n");
+        return 0;
+    }
+
+    // Parent process waits for both child processes to finish
+    int status;
+    waitpid(pid1, &status, 0);  // Wait for first child
+    waitpid(pid2, &status, 0);  // Wait for second child
+
+    return 0;
 }
